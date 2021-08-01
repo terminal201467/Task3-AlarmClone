7be43a70ab9af221c93c1e41fbef01032ff5ab24
MJ <terminal201467@gmail.com>
2021-08-01 17:04:36 +0800

    Initial Commit

---
+++ SomeNewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeNewProject
+//
+//  Created by Jhen Mu on 2021/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
